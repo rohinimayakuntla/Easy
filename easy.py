1cf3f722a257f424845ac6d70535ca7e4962b3db
rohinimayakuntla <133483107+rohinimayakuntla@users.noreply.github.com>
2023-12-01 18:16:19 +0530

    easy.py

--- easy.py
+++ easy.py
@@ -1,8 +1,8 @@
 def length_of_last_word(s):
-    words = s.split()
-    if len(words) == 0:
+    str = s.split()
+    if len(str) == 0:
         return 0
-    return len(words[-1])
+    return len(str[-1])
 user_input = input("Enter a string: ")
 output = length_of_last_word(user_input)
 print("Length of the last word:", output)
